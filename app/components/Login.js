0dd3375d6f7b96fda17343698f3fd6d864cdc579
Angus Yip <ayip002@gmail.com>
2018-01-14 11:53:49 -0800

    Almost

--- app/components/Login.js
+++ app/components/Login.js
@@ -7,11 +7,11 @@ const Login = ({ toggleLogin, loggedIn }) => (
       to="/"
       onClick={() => toggleLogin(!loggedIn)}
     >
+      <a href="#">key path='~/Downloads' (default)</a>
       <div class="google-btn">
         <div class="google-icon-wrapper">
-          <img class="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
         </div>
-        <p class="btn-text"><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sign in with Google</b></p>
+        <p class="btn-text"><b>Sign in with your key</b></p>
       </div>
     </Link>
   </div>
--- app/components/Signup.js
+++ app/components/Signup.js
@@ -1,9 +1,123 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-const Signup = () => (
-  <div>
-    <p>This is the sign up page</p>
-  </div>
-);
+class Signup extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {"step":0};
+    this.handleNextState = this.handleNextState.bind(this);
+    const { toggleLogin } = this.props;
+  };
+
+  handleNextState () {
+    this.setState({"step": this.state.step + 1});
+  };
+
+  render () {
+    const { step } = this.state;
+
+    const shuffle = (a) => {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    };
+
+    var wordPairs = [
+      "seashore jump",
+      "town mailbox",
+      "reproduce glib",
+      "grass serve",
+      "horrible gaudy",
+      "pollution search",
+      "fire mate",
+      "shelf stocking",
+      "waste worry",
+      "veil lean"
+    ];
+    shuffle(wordPairs);
+
+    const block = wordPairs.map((words, i) => (
+      <a href="#">
+        { words }
+        <br />
+      </a>
+    ));
+
+    const content = [
+      <div>
+        <h2>Enter account information</h2>
+        <a href="#">Username:</a>
+        <br />
+        <input />
+        <br />
+        <a href="#">Email (Optional):</a>
+        <br />
+        <input />
+        <br />
+        <a href="#">Passphrase:</a>
+        <br />
+        <input />
+        <br />
+        <a href="#">Enter pass again:</a>
+        <br />
+        <input />
+        <br />
+        <br />
+        <br />
+      </div>,
+      <div>
+        <h2><b>IMPORTANT!</b> Save this seed for account recovery</h2>
+        <h2>If you lose this you will lose means to recover your account, forever!</h2>
+        <br />
+        <br />
+        <a href="#">1</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;
+        <a href="#">11</a>
+        <br />
+        <Button>
+          {block}
+        </Button>
+        <br />
+        <a href="#">10</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;
+        <a href="#">20</a>
+        <br />
+        <br />
+        <br />
+      </div>,
+      <div>
+        <h2>Welcome to NCB!</h2>
+        <Button href="/login">Log in now</Button>
+      </div>
+    ];
+
+    const buttonText = [
+      "Next",
+      "Generate P/P Key Pair"
+    ]
+
+    return (
+      <div>
+        {content[step]}
+        <Button onClick={this.handleNextState}>{buttonText[step]}</Button>
+        <br />
+        <br />
+        <br />
+      </div>
+    );
+  };
+};
 
 export default Signup;

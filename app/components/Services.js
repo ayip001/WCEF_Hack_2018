b3c4d8dbf7270541ba069e8e3132dc0265c757ad
Angus Yip <ayip002@gmail.com>
2018-01-14 08:48:42 -0800

    Merge

--- app/components/Services.js
+++ app/components/Services.js
@@ -5,16 +5,10 @@ import drive from '../assets/images/experian_square.jpg';
 import dropbox from '../assets/images/experian_square.jpg';	
 
 const Services = () => (
-<<<<<<< HEAD
   <div style={{"border-style":"solid","border-color":"#00ff00",}}>
     <img src={aws} style={{"width":"200px","height":"200px",}} />
     <img src={drive} style={{"width":"200px","height":"200px",}} />
     <img src={dropbox} style={{"width":"200px","height":"200px",}} />
-    
-=======
-  <div>
-    img placeholder
->>>>>>> c96201dfeb8fbe9a89ecd2331dc57847e819aa87
   </div>
 );
 
